a3cfe1150ed8af558f98be4701f31fcb4db28a6f
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2025-03-25 13:23:32 +0100

    refixed homepage responsiveness

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="2xl:bg-[#3D3CC9] 2xl:flex border-t border-white 2xl:text-white 2xl:bottom-0 2xl:h-48 pt-7 z-50 footer">
-      <div className="z-40">
+    <footer className="bg-[#3D3CC9] 2xl:flex border-t border-white text-white 2xl:bottom-0 2xl:h-48 pt-7 z-50 footer">
+      <div className="z-40 relative">
         <h1 className=" font-[800] scale-y-150 tracking-wide 2xl:text-xl mx-5 footerh">
           Reader's Assisstant AI
         </h1>
@@ -14,18 +14,18 @@ const Footer = () => {
           can focus on what matters.
         </p>
       </div>
-      <div className="2xl:flex 2xl:flex-col 2xl:justify-between 2xl:h-[120px] 2xl:text-[1.2em] mt-7 footerl relative">
+      <div className="flex flex-col justify-between 2xl:h-[120px] 2xl:text-[1.2em] mt-7 footerl relative ml-5">
         <Link href={"/"} className="2xl:underline">
           Guidelines
         </Link>
         <Link href={"/"} className="2xl:underline">
-          Analyze Text
-        </Link>
-        <Link href={"/"} className="2xl:underline">
           Text-to-Audio
         </Link>
+        <Link href={"/"} className="2xl:underline">
+          Question and Answer
+        </Link>
       </div>
-      <div className="2xl:flex 2xl:flex-col 2xl:justify-between 2xl:h-[120px] 2xl:text-[1.2em] 2xl:mt-7 2xl:ml-28 footerl2">
+      <div className="flex flex-col 2xl:relative absolute right-2 bottom-[100px] justify-between 2xl:h-[120px] 2xl:text-[1.2em] 2xl:mt-7 2xl:ml-28 footerl2">
         {/* <Link href={"/"} className="2xl:underline">
           Account Settings
         </Link> */}
@@ -36,18 +36,22 @@ const Footer = () => {
           Developer Profile
         </Link>
       </div>
-      <form className="2xl:mt-7 2xl:ml-28 footerform">
-        <div className="2xl:flex f1">
-          <input type="checkbox" className="2xl:border 2xl:border-white 2xl:rounded 2xl:w-4" />
+      <form className="2xl:mt-7 2xl:ml-28 footerform flex 2xl:flex-col justify-between mx-3 py-2">
+        <div className="flex f1">
+          <input
+            type="checkbox"
+            className="2xl:border 2xl:border-white 2xl:rounded 2xl:w-4"
+          />
           <p className="2xl:ml-2 2xl:text-[1.1em]">Was this helpful to you?</p>
         </div>
 
         <div className="flex">
-          <input type="checkbox" className="2xl:border 2xl:border-white 2xl:rounded 2xl:w-4" />
+          <input
+            type="checkbox"
+            className="2xl:border 2xl:border-white 2xl:rounded 2xl:w-4"
+          />
           <p className="2xl:ml-2 2xl:text-[1.1em]">Did you enjoy it?</p>
         </div>
-
-       
       </form>
     </footer>
   );
